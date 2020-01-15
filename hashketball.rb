7a092c55a02c62f0b79732d0f0625d4577533e48
Diana Wendt <dianakw@gmail.com>
2020-01-15 17:55:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,6 +133,7 @@ def team_colors(team)
   p "checking for #{team}"
   game_hash.each do |(side, info_hash)|
     p side
+    p info_hash
     if info_hash[:team_name] = team
       p "#{info_hash[:team_name]} is #{team}!"
       p info_hash[:colors]
