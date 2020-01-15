867337125d9f69c4c92cfd7dae30965a7c872c0e
Diana Wendt <dianakw@gmail.com>
2020-01-15 19:46:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,9 +162,9 @@ end
 
 def big_shoe_rebounds
   #find player with largest shoe size and return their rebounds
-  game_hash.reduce(0) do |memo, (side, info_hash)|
+  game_hash.reduce({}) do |memo, (side, info_hash)|
     largest_shoe_hash = info_hash[:players].max_by {|player_hash| player_hash[:shoe]}
-    memo = largest_shoe_hash[:shoe] if largest_shoe_hash[:shoe] > memo
+    memo = largest_shoe_hash if largest_shoe_hash[:shoe] > memo[:shoe]
     memo
   end
 end
